705c6885b6c545ad0c07321c13ffee0e66abfe34
Mikhail Povolotsky <mpovolot@gmail.com>
2006-09-13 11:09:14 +0000

    Optical generation of electron and hole is added

---
+++ include/core/drift-diffusion/models/OpticalGeneration.h
@@ -0,0 +1,57 @@
+// $Id: ExcitonGeneration.h 70 2006-07-14 16:57:35Z maufder $
+
+#ifndef _OPTICALGENERATION_H_
+#define _OPTICALENERATION_H_
+
+#include "RecombinationModelInterface.h"
+#include "TypeDefs.h"
+
+
+//! Implementation of optical generation
+/*!
+ * This class implements optical generation processes that can be
+ * modeled by \f[G_{x}= G]
+ */
+class OpticalGeneration : public RecombinationModelInterface
+{
+
+  public:
+
+    //! Constructor
+    OpticalGeneration(void);
+
+    //! Destructor
+    virtual ~OpticalGeneration(void) {};
+
+    //! \copydoc RecombinationModelInterface::get_net_recombination_rates()
+    void get_net_recombination_rates(double& recomb_e, double& recomb_h);
+
+    /*!
+     * \copydoc
+     * RecombinationModelInterface::get_net_recombination_rate_derivatives()
+     */
+    void get_net_recombination_rate_derivatives(
+        std::vector<double>& recomb_e, std::vector<double>& recomb_h);
+
+    //! Set the direct recombination parameters
+    void set_parameters(double C);
+
+    //! \copydoc RecombinationModelInterface::set_model_options()
+    virtual void set_model_options(const ModelOptions& options);
+    
+  private:
+
+    //! Generation rate parameter
+    double _G;
+
+};
+
+
+inline
+void
+OpticalGeneration::set_parameters(double G)
+{
+  _G = G;
+}
+
+#endif // _OPTICALGENERATION_H_
---
+++ src/core/drift-diffusion/models/OpticalGeneration.C
@@ -0,0 +1,38 @@
+#include "OpticalGeneration.h"
+#include "DriftDiffusionProperties.h"
+
+
+#include <typeinfo>
+
+OpticalGeneration::OpticalGeneration(void)
+  : RecombinationModelInterface(),
+    _G(1e-10)
+
+{
+}
+
+void
+OpticalGeneration::set_model_options(const ModelOptions& options)
+{
+ 
+}
+
+void
+OpticalGeneration::get_net_recombination_rates(double& recomb_e,
+    double& recomb_h)
+{
+  
+
+  recomb_e = recomb_h = -_G;
+}
+
+void
+OpticalGeneration::get_net_recombination_rate_derivatives(
+    std::vector<double>& recomb_e, std::vector<double>& recomb_h)
+{
+ 
+
+  recomb_e[0] = recomb_h[0] = 0;
+  recomb_e[1] = recomb_h[1] = 0;
+  recomb_e[2] = recomb_h[2] = 0;
+}
