7c65a2466e56723131b72cf25b72424e933cca1b
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2007-04-03 14:07:12 +0000

    * bug fix in FiniteElement.C

--- src/base/common/FiniteElement.C
+++ src/base/common/FiniteElement.C
@@ -18,6 +18,7 @@ FiniteElement<Dim, T>::reinit(const Elem* elem, const unsigned int side)
   unsigned int dim = FE<Dim, T>::qrule->get_dim();
   unsigned int n_points = FE<Dim, T>::JxW.size();
 
+  double x0 = _length_scaling / _mesh_units;
   double x0_inv = _mesh_units / _length_scaling;
 
   double J = x0_inv;
@@ -33,13 +34,18 @@ FiniteElement<Dim, T>::reinit(const Elem* elem, const unsigned int side)
   {
     case TiberCad::CYLINDRICAL:
       for (unsigned int i = 0; i < n_points; i++)
-        FE<Dim, T>::JxW[i] *= 2 * M_PI * FE<Dim, T>::xyz[i](0) * x0_inv * J;
+        FE<Dim, T>::JxW[i] *= 2 * M_PI * abs(FE<Dim, T>::xyz[i](0)) * x0_inv * J;
       break;
     default:
       for (unsigned int i = 0; i < n_points; i++)
         FE<Dim, T>::JxW[i] *= J;
       break;
   }
+  
+  //if (FE<Dim, T>::calculate_dphi)
+    for (unsigned int i = 0; i < n_points; i++)
+      for (unsigned int j = 0; j < elem->n_nodes(); j++)
+        FE<Dim, T>::dphi[j][i] *= x0;
 
 }
 
@@ -69,7 +75,7 @@ FiniteElement<Dim, T>::reinit(const Elem* elem, const vector<Point>* points)
   {
     case TiberCad::CYLINDRICAL:
       for (unsigned int i = 0; i < n_points; i++)
-        FE<Dim, T>::JxW[i] *= 2 * M_PI * FE<Dim, T>::xyz[i](0) * x0_inv * J;
+        FE<Dim, T>::JxW[i] *= 2 * M_PI * abs(FE<Dim, T>::xyz[i](0)) * x0_inv * J;
       break;
     default:
       for (unsigned int i = 0; i < n_points; i++)
