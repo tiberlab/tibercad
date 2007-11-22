ccbb1a5cf089f62926b48c42513544d61f79bf36
Mikhail Povolotsky <mpovolot@gmail.com>
2007-11-22 12:08:17 +0000

    SLEPC 2.3.2

--- configure.sh
+++ configure.sh
@@ -5,7 +5,7 @@ export CC=gcc-4.1.1
 
 ./configure --with-boost-prefix=/usr/pack/boost-1.33.1-ma \
   --with-libmesh-prefix=/usr/pack/libmesh-0.6.0_20070607cvs-ma \
-  --with-petsc-prefix=/usr/pack/petsc-2.3.2-ma \
+  --with-petsc-prefix=/usr/pack/petsc-2.3.3-ma \
   --with-petsc-arch=linux-gnu-complex-opt \
-  --with-slepc-prefix=/usr/pack/slepc-2.3.2-ma \
+  --with-slepc-prefix=/usr/pack/slepc-2.3.3-ma \
   --disable-modules
