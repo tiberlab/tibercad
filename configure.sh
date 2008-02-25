f2d45f230829710b955da2c1d08ee16c9362122d
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2008-02-25 09:55:45 +0000

    * returned to old libmesh

--- configure.sh
+++ configure.sh
@@ -5,12 +5,12 @@ export CC=gcc-4.1.1
 export FC=ifort-9.0
 
 ./configure --with-boost-prefix=/usr/pack/boost-1.33.1-ma \
-  --with-petsc-prefix=/usr/pack/petsc-2.3.3-ma \
+  --with-petsc-prefix=/usr/pack/petsc-2.3.0-ma \
   --with-petsc-arch=linux-gnu-complex \
-  --with-slepc-prefix=/usr/pack/slepc-2.3.3-ma \
+  --with-slepc-prefix=/usr/pack/slepc-2.3.0-ma \
   --with-mkl=/usr/pack/intel_mkl-8.0-ma \
   --disable-modules \
   --enable-pardiso \
   --enable-dftb \
   --enable-hetero \
-  --with-libmesh-prefix=/usr/pack/libmesh-0.6.2-ma 
+  --with-libmesh-prefix=/usr/pack/libmesh-0.6.0_20070607cvs-ma 
