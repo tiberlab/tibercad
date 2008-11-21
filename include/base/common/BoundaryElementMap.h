9922b86b56ba849b2dac38b92d93cfab5812485e
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2008-11-21 17:40:46 +0000

    * added files missing in last commit

---
+++ include/base/common/BoundaryElementMap.h
@@ -0,0 +1,232 @@
+// $Id$
+
+#ifndef _BOUNDARYELEMENTMAP_H_
+#define _BOUNDARYELEMENTMAP_H_
+
+#include "TypeDefs.h"
+
+#include <map>
+#include <set>
+#include <vector>
+
+class Elem;
+class Boundary;
+
+
+//! Contains a map connecting boundaries to set of elems
+class BoundaryElementMap
+{
+
+  public:
+
+    //! A typedef for convenience
+    typedef std::set<const Elem*> SetType;
+
+    //! A typedef for the map
+    typedef std::map<const Boundary*, SetType> MapType;
+
+    //! an iterator for the map
+    typedef MapType::iterator map_iterator;
+
+
+    //! An iterator that iterates over all elements
+    /*!
+     * This iterator will iterate over teh elements of one or
+     * all boundaries. It is possible that an element is returned
+     * more than once. This happens if different contacts touch in a
+     * set of nodes.
+     */
+    class iterator
+    {
+      public:
+
+        //! Default constructor
+        iterator(void) : _value(NULL) {};
+
+        //! Constructor
+        iterator(const BoundaryElementMap* container,
+            const Boundary* bd = NULL, bool begin = false) :
+          _container(container), _bd(bd), _value(NULL)
+        {
+          if (begin)
+          {
+            if (bd != NULL)
+              _map_iter = _container->_map.find(bd);
+            else
+              _map_iter = _container->_map.begin();
+
+            if (_map_iter != _container->_map.end())
+            {
+              _set_iter = (_map_iter->second).begin();
+              if (_set_iter != (_map_iter->second).end())
+                _value = &(*_set_iter);
+            }
+          }
+        }
+
+        //! Copy constructor
+        iterator(const iterator& it) :
+          _container(it._container), _bd(it._bd), _value(it._value),
+          _set_iter(it._set_iter), _map_iter(it._map_iter)
+        {}
+
+        //! Prefix increment
+        iterator& operator++(void)
+        {
+          if (_value != NULL)
+          {
+            _value = NULL;
+
+            if (++_set_iter != (_map_iter->second).end())
+              _value = &(*_set_iter);
+            else if (_bd == NULL)
+            {
+              if (++_map_iter != _container->_map.end())
+                if ((_set_iter = (_map_iter->second).begin()) != 
+                    (_map_iter->second).end())
+                  _value = &(*_set_iter);
+            }
+          }
+          return *this;
+        }
+
+        iterator& operator=(const iterator& rhs)
+        {
+          _container = rhs._container;
+          _bd = rhs._bd;
+          _value = rhs._value;
+          _set_iter = rhs._set_iter;
+          _set_iter = rhs._set_iter;
+          _map_iter = rhs._map_iter;
+          return *this;
+        }
+
+        bool operator==(const iterator& rhs)
+        {
+          return (_value == rhs._value);
+        }
+
+        bool operator!=(const iterator& rhs)
+        {
+          return (_value != rhs._value);
+        }
+
+        const Elem* operator*(void)
+        {
+          return *_value;
+        }
+
+        const Boundary* get_boundary(void)
+        {
+          return (_value == NULL) ? NULL : _map_iter->first;
+        }
+
+
+      private:
+
+        const BoundaryElementMap* _container;
+        const Boundary* _bd;
+        const Elem* const* _value;
+
+        SetType::const_iterator _set_iter;
+        MapType::const_iterator _map_iter;
+    };
+
+
+
+
+
+    //! Constructor
+    BoundaryElementMap(void);
+
+    //! Get the set of elements for a certain boundary 
+    const SetType& get(const Boundary* boundary) const;
+
+
+    //! Add a single element
+    void add(const Boundary* boundary, const Elem* elem);
+
+    //! Find the boundaries corresponding to an element
+    /*!
+     * \return true if the elem is on a boundary
+     */
+    bool find(const Elem* elem, std::set<const Boundary*>& bds) const;
+
+    //! Find the boundaries corresponding to an element
+    /*!
+     * \return true if the elem is on a boundary
+     */
+    bool find(const Elem* elem, std::vector<const Boundary*>& bds) const;
+
+    //! The start of the map
+    map_iterator map_begin(void);
+
+    //! The end of the map
+    map_iterator map_end(void);
+
+    //! The first element
+    const iterator elements_begin(const Boundary* bd = NULL) const;
+
+    //! The last element
+    const iterator elements_end(const Boundary* bd = NULL) const;
+
+
+  private:
+
+    //! The real map
+    MapType _map;
+
+    //! The empty element set
+    const static SetType _empty_set;
+
+    //! Let the iterator access the our map
+    friend class iterator;
+};
+
+
+//
+// inline methods
+//
+
+inline
+void
+BoundaryElementMap::add(const Boundary* boundary, const Elem* elem)
+{
+  _map[boundary].insert(elem);
+}
+
+
+inline
+BoundaryElementMap::map_iterator
+BoundaryElementMap::map_begin(void)
+{
+  return _map.begin();
+}
+
+
+
+inline
+BoundaryElementMap::map_iterator
+BoundaryElementMap::map_end(void)
+{
+  return _map.end();
+}
+
+
+inline
+const BoundaryElementMap::iterator
+BoundaryElementMap::elements_begin(const Boundary* bd) const
+{
+  return iterator(this, bd, true);
+}
+
+
+inline
+const BoundaryElementMap::iterator
+BoundaryElementMap::elements_end(const Boundary* bd) const
+{
+  return iterator(this, bd, false);
+}
+
+
+#endif // _BOUNDARYELEMENTMAP_H_
