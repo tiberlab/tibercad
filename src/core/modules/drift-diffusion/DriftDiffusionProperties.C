3b159d8a409842e07add0c20f7587cbc5c12c22f
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2008-12-27 11:33:03 +0000

    * bug fix in DriftDiffusionProperties

--- src/core/modules/drift-diffusion/DriftDiffusionProperties.C
+++ src/core/modules/drift-diffusion/DriftDiffusionProperties.C
@@ -376,11 +376,15 @@ DriftDiffusionProperties::do_init_alloy(const PhysicalModelInterface* comp_A,
       scB->_pyropolarization, xa);
 
 
-  PhysicalModelInterface::destroy(_thermoelectric_power);
-  _thermoelectric_power = create_submodel_copy(scA->_thermoelectric_power);
-  _thermoelectric_power->set_driftdiffusionproperties(this);
-  _thermoelectric_power->init_alloy(scA->_thermoelectric_power,
-      scB->_thermoelectric_power, xa);
+  if (scA->_thermoelectric_power != NULL)
+  {
+    assert(scB->_thermoelectric_power != NULL);
+    PhysicalModelInterface::destroy(_thermoelectric_power);
+    _thermoelectric_power = create_submodel_copy(scA->_thermoelectric_power);
+    _thermoelectric_power->set_driftdiffusionproperties(this);
+    _thermoelectric_power->init_alloy(scA->_thermoelectric_power,
+        scB->_thermoelectric_power, xa);
+  }
 
 }
 
