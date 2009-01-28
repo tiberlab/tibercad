c9a8def5976d4af7e9f3f7a05f62be4f27d7e9cf
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2009-01-28 06:54:59 +0000

    * a fix for test04 (I hope)

--- src/core/solver-interfaces/petsc/TiberPetscNonlinearSolver.C
+++ src/core/solver-interfaces/petsc/TiberPetscNonlinearSolver.C
@@ -65,8 +65,8 @@ extern "C"
 
     // check for convergence
     //if ((reason < 0) && (reason != -3) && (reason != -4))
-    if (reason < 0)
-    //if ((reason < 0) && (reason != -3))
+    //if (reason < 0)
+    if ((reason < 0) && (reason != -3))
     {
 #ifndef DEBUG
       std::cout << std::endl << std::flush;
