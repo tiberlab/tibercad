6a2afed0a50d48176409d6c5286a15d365afd9ed
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2009-09-30 08:40:38 +0000

    * use newly compiled libraries

--- configure.sh
+++ configure.sh
@@ -5,9 +5,9 @@ export CC=gcc-4.1.1
 export FC=ifort-9.0
 
 ./configure --with-boost-prefix=/usr/pack/boost-1.33.1-ma \
-  --with-petsc-prefix=/usr/pack/petsc-2.3.3-ma \
+  --with-petsc-prefix=/usr/pack/tibercad_dev-1.3-ma/petsc-2.3.3-p13 \
   --with-petsc-arch=linux-gnu-complex \
-  --with-slepc-prefix=/usr/pack/slepc-2.3.3-ma \
+  --with-slepc-prefix=/usr/pack/tibercad_dev-1.3-ma/slepc-2.3.3 \
   --with-mkl=/usr/pack/intel_mkl-8.0-ma \
   --with-subversion=svn-1.6.5 \
   --disable-modules \
@@ -15,4 +15,4 @@ export FC=ifort-9.0
   --enable-dftb \
   --enable-uptight\
   --enable-hetero \
-  --with-libmesh-prefix=/usr/pack/libmesh-0.6.1-ma 
+  --with-libmesh-prefix=/usr/pack/tibercad_dev-1.3-ma/libmesh-0.6.1 
