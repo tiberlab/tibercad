13dc9b63a76d2e57a09cf73f4297a4ca41689595
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2010-09-24 15:19:10 +0000

    * added Tensor2Gen * RealVectorValue

--- include/base/math/TensorOperators.h
+++ include/base/math/TensorOperators.h
@@ -13,11 +13,24 @@ inline
 RealVectorValue operator*(const Tensor2Sym& A, const RealVectorValue& x)
 {
   RealVectorValue v(0);
+  v(0) += A(1, 1) * x(0) + A(2, 1) * x(1) + A(3, 1) * x(2);
+  v(1) += A(1, 2) * x(0) + A(2, 2) * x(1) + A(3, 2) * x(2);
+  v(2) += A(1, 3) * x(0) + A(2, 3) * x(1) + A(3, 3) * x(2);
+
+  return v;
+}
+
+
+//! Multiplication of generic 2-rank tensor with vector
+inline
+RealVectorValue operator*(const Tensor2Gen& A, const RealVectorValue& x)
+{
+  RealVectorValue v(0);
   for (unsigned int i = 0; i < 3; ++i)
   {
-    v(0) += A(i+1, 1) * x(i);
-    v(1) += A(i+1, 2) * x(i);
-    v(2) += A(i+1, 3) * x(i);
+    v(0) += A(1, i+1) * x(i);
+    v(1) += A(2, i+1) * x(i);
+    v(2) += A(3, i+1) * x(i);
   }
 
   return v;
