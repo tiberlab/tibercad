8b2273a182dc0416d3cecfe9492cbe9893d79a6f
Giuseppe Romano <giusepperomano82@gmail.com>
2010-12-08 04:31:16 +0000

    Add some files

---
+++ src/core/modules/elasticity/models/Substrate.h
@@ -0,0 +1,91 @@
+// $Id: Clamp.h 2124 2010-10-22 14:00:17Z gromano $
+
+#ifndef _SUBSTRATE_H_
+#define _SUBSTRATE_H_
+
+#include "BoundaryModel.h"
+
+#include "point.h"
+#include "tensor_value.h"
+#include "vector_value.h"
+
+
+
+class Elem;
+
+
+//! The base class for Poisson boundary conditions
+class Substrate : public BoundaryModel
+{
+
+  public:
+
+    //! Destructor
+    ~Substrate(void) {};
+
+    //! Creator function
+    static Substrate* create(const ModelOptions& options);
+
+
+    //! Calculate for a point on the given side
+    virtual void calculate(const Elem* elem, unsigned int side,
+			   const Point& point);
+
+
+  protected:
+
+    //! Initialize
+  virtual void do_init(void){};
+
+    /* In some cases it might be useful to reimplement this: */
+    // virtual void do_init_interface(const PhysicalModelInterface* comp_A,
+    //         const PhysicalModelInterface* comp_B);
+
+
+    /* This is not used here: */
+    // virtual void read_database(void);
+
+
+    /* We do not use this here: */
+    // virtual void read_interface_database(void);
+
+
+    //! Create a new object of the same type
+    virtual PhysicalModelInterface* create_new(void) const;
+
+
+  private:
+
+    //! Constructor
+    Substrate(const ModelOptions& options);
+
+   
+};
+
+
+
+inline
+Substrate::Substrate(const ModelOptions& options) :
+  BoundaryModel(options)
+{
+}
+
+
+
+inline
+Substrate*
+Substrate::create(const ModelOptions& options)
+{
+  return new Substrate(options);
+}
+
+
+
+inline
+PhysicalModelInterface*
+Substrate::create_new(void) const
+{
+  return new Substrate(get_options());
+}
+
+#endif // _POISSONDIRICHLET_H_
