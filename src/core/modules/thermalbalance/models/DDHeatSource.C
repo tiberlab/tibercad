139e53b8a4c21c3022fedb9b61cf8274aac34638
Giuseppe Romano <giusepperomano82@gmail.com>
2011-02-10 21:05:30 +0000

    bug fixed

--- src/core/modules/thermalbalance/models/DDHeatSource.C
+++ src/core/modules/thermalbalance/models/DDHeatSource.C
@@ -24,7 +24,7 @@ DDHeatSource::do_init(void)
 {
 
   
-  string dd_simul_name = get_options().get_option("simulation_name", driftdiffusion);
+  string dd_simul_name = get_options().get_option("simulation_name", "driftdiffusion");
   _simul = SimulationInterface::find_simulation(dd_simul_name);
 
   if ( _simul == NULL)
