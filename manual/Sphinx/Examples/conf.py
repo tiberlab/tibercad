aaf8a1a9133b61b8851a341c579920fae0f93dc1
Alessio Molinari <palanthir@tiscali.it>
2011-05-03 14:33:38 +0000

    Change to get a more specific filename

--- manual/Sphinx/Examples/conf.py
+++ manual/Sphinx/Examples/conf.py
@@ -178,7 +178,7 @@ latex_font_size = '12pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'TiberCAD.tex', u' Examples Book ',
+  ('index', 'TiberCAD-Examples.tex', u' Examples Book ',
    u'TiberLAB srl', 'manual'),
 ]
 
