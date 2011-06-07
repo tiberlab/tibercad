80c0e6b2daa6123796a1e722242f7d4fe031717a
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2011-06-07 10:35:21 +0000

    * exponential traps should now work

--- src/core/models/ExponentialDOS.C
+++ src/core/models/ExponentialDOS.C
@@ -42,34 +42,40 @@ ExponentialDOS::get_occupied_density(double E, double kT) const
 {
   double sum = 0;
 
-  // We divide integration into 3 parts
-  double g = min(5 * kT, 5 * _alpha);
-  if (E - g < 0)
-  {
-    sum += exp((E - g) / _alpha);
-    double emax = 0;
-
-    if (E + g < 0)
-    {
-      if (kT != _alpha)
-      {
-        double tmp = (E + g) * (kT - _alpha) / (kT * _alpha);
-        sum += kT / (kT - _alpha) * (1 - exp(tmp)) * exp(E/kT);
-      }
-      emax = E + g;
-    }
-
-    double emin = E - g;
-    int steps = 25;
-    double h = (emax - emin) / steps;
-    for (int i = 0; i < steps; i++)
-    {
-      double e = emin + h * i;
-      sum += h * _get_value(e, E, kT);
-    }
-  }
+  if (_alpha < 0.5 * kT)
+    // it's almost a delta
+    sum = 1.0 / (1.0 + exp(-E / kT));
   else
-    sum = 1;
+  {
+    // We divide integration into 3 parts
+    double g = min(5 * kT, 5 * _alpha);
+    if (E - g < 0)
+    {
+      sum += exp((E - g) / _alpha);
+      double emax = 0;
+
+      if (E + g < 0)
+      {
+        if (kT != _alpha)
+        {
+          double tmp = (E + g) * (kT - _alpha) / (kT * _alpha);
+          sum += kT / (kT - _alpha) * (1 - exp(tmp)) * exp(E/kT);
+        }
+        emax = E + g;
+      }
+
+      double emin = E - g;
+      int steps = 25;
+      double h = (emax - emin) / steps;
+      for (int i = 0; i < steps; i++)
+      {
+        double e = emin + h * i;
+        sum += h * _get_value(e, E, kT);
+      }
+    }
+    else
+      sum = 1;
+  }
 
   return sum;
 }
@@ -79,6 +85,16 @@ double
 ExponentialDOS::get_occupied_density_derivative(double E, double kT) const
 {
   double der = get_occupied_density(E, kT);
-  der -= 1.0 / (1 + exp(-E / kT));
-  return der / _alpha;
+  if (_alpha < 0.5 * kT)
+  {
+    // it's almost a delta
+    der /= kT * (1 + exp(-E / kT));
+    der *= exp(-E/kT);
+  }
+  else
+  {
+    der -= 1.0 / (1 + exp(-E / kT));
+    der /= _alpha;
+  }
+  return der;
 }
