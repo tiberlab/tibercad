e58f54358a7ffe71fc11a9bae8bba6587a18a5f1
Giuseppe Romano <giusepperomano82@gmail.com>
2011-07-18 04:14:12 +0000

    test

--- src/core/modules/common/ConstantThermalConductivity.C
+++ src/core/modules/common/ConstantThermalConductivity.C
@@ -7,7 +7,7 @@
 #include "RotatedCrystal.h"
 
 
-TIBER_MODULE(ConstantThermalConductivity, ThermalConductivity, constant)
+TIBER_MODULE(ConstantThermalConductivity, thermal_conductivity, constant)
 
 using namespace std;
 
