a6e1241eb259e4873fd90f2a5f51fe4310fe5b74
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2011-10-01 19:21:15 +0000

    * added a bodyforce model for thermal stress

---
+++ src/core/modules/elasticity/models/ThermalStress.C
@@ -0,0 +1,102 @@
+// $Id: ThermalStress.C 2451 2011-03-05 14:45:46Z maufder $
+
+#include "ThermalStress.h"
+#include "Material.h"
+#include "RotatedCrystal.h"
+#include "Database.h"
+#include "SimulationOptions.h"
+#include "TensorOperators.h"
+
+// The first string is the class name, the second one
+// is the type of the model (here it is a bulk model),
+// the third one is the specific model implementation.
+// The library name will then be bulk_default.so
+
+TIBER_MODULE(ThermalStress, body_force, thermal_stress)
+
+using namespace std;
+
+
+ThermalStress::ThermalStress(const ModelOptions& options) :
+    BodyForceModel(options)
+{
+}
+
+ThermalStress::~ThermalStress(void)
+{
+
+}
+
+
+void
+ThermalStress::do_init(void)
+{
+
+  RealGradient body_force(0);
+  
+ //Get reference lattice
+  _ref_temp = get_option("reference_temperature", SimulationOptions::temperature);
+ 
+  get_parameter("thermal_expansion_coefficient", _alpha);
+
+ 
+  std::string temp_sim = get_option("thermal_simulation", "");
+
+  _temp.set_simulation(temp_sim);
+}
+
+
+
+void
+ThermalStress::read_database(void)
+{
+
+  const Database& db = get_database();
+  db.set_section("lattice");
+
+  vector<double> alpha;
+  db.get("thermal_coefficient", alpha);
+
+  switch (alpha.size())
+  {
+    case 1:
+      _alpha(0) = _alpha(1) = _alpha(2) = alpha[0];
+      break;
+    case 2:
+      _alpha(0) = _alpha(1) = alpha[0];
+      _alpha(2) = alpha[1];
+      break;
+    case 3:
+      _alpha(0) = alpha[0];
+      _alpha(1) = alpha[1];
+      _alpha(2) = alpha[2];
+      break;
+    default:
+      break;
+  }
+
+}
+
+ 
+
+void
+ThermalStress::calculate(const Elem* elem, const Point& point)
+{
+  // get temperature
+  double deltaT = _temp.get_temperature(elem, point) - _ref_temp;
+
+  const RotatedCrystal& cr = get_material()->get_rotated_crystal();
+
+
+  // compute thermally induced strain
+  RealTensor strain(0);
+  strain(0,0) = -_alpha(0) * deltaT;
+  strain(1,1) = -_alpha(1) * deltaT;
+  strain(2,2) = -_alpha(2) * deltaT;
+
+  // rotate
+  strain = cr.RotMatrix * (strain * cr.RotMatrix.transpose());
+
+  set_strain_source(strain);
+}
+
---
+++ src/core/modules/elasticity/models/ThermalStress.h
@@ -0,0 +1,72 @@
+// $Id: ThermalStress.h 2451 2011-03-05 14:45:46Z maufder $
+
+#ifndef _THERMALSTRESS_H_
+#define _THERMALSTRESS_H_
+
+#include "BodyForceModel.h"
+
+#include "TemperatureInterface.h"
+#include "vector_value.h"
+#include "tiber_dll.h"
+
+class Elem;
+
+
+
+//! The base class for Poisson boundary conditions
+class TBDLLOCAL ThermalStress : public BodyForceModel
+{
+
+  public:
+ 
+  //! Destructor
+  ~ThermalStress(void);
+  
+  //! Creator function
+  static ThermalStress* create(const ModelOptions& options);
+  
+  virtual void calculate(const Elem* elem, const Point& point);
+
+  protected:
+
+    //! Initialize
+    virtual void do_init(void);
+
+
+    //! Read expansion coefficients
+    virtual void read_database(void);
+
+
+  private:
+  
+
+    //! Constructor
+    ThermalStress(const ModelOptions& options);
+
+    //! Thermal expansion coefficients for the crystal directions
+    RealVectorValue _alpha;
+
+    //! A reference temperature
+    double _ref_temp;
+
+    //! From where to get the temperature
+    TemperatureInterface _temp;
+
+
+  
+};
+
+
+
+
+inline
+ThermalStress*
+ThermalStress::create(const ModelOptions& options)
+{ 
+  return new  ThermalStress(options);
+}
+
+
+
+
+#endif // _THERMALSTRESS_H_
