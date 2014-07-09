cbc81d2bc60208cb08b7d9855a470e239483ce7c
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2014-07-09 17:14:59 +0000

    * change to real MPI based development libraries

--- configure.sh
+++ configure.sh
@@ -1,31 +1,54 @@
 #!/bin/sh
- 
-export CXX=g++-4.6.0
-export CC=gcc-4.6.0
-export FC=ifort-11.1
-export FCFLAGS="-fPIC -fexceptions -gcc-name=${CC} -gxx-name=${CXX} -nofor-main"
 
-SDKDIR=/usr/pack/tibercad_sdk-2.2-ma
-ARCH=x86_64-linux
+ARCH=`uname -m`
+if [ $ARCH == "i686" ]
+then
+  sepparch=i686-debian-linux4.0
+else
+  sepparch=amd64-debian-linux5.0
+  FCFLAGS="-fPIC"
+fi 
+
+BUILDDIR=/usr/pack/tibercad_dev-2.2-ma/extern
+PETSC_DIR=${BUILDDIR}/petsc-3.0.0-p12
+SLEPC_DIR=${BUILDDIR}/slepc-3.0.0-p7
+
+#export CXX=g++-4.6.0
+#export CC=gcc-4.6.0
+#export FC=ifort-11.1
+export CXX=/usr/pack/tibercad_dev-2.2-ma/${ARCH}-linux/bin/mpicxx
+export CC=/usr/pack/tibercad_dev-2.2-ma/${ARCH}-linux/bin/mpicc
+export FC=/usr/pack/tibercad_dev-2.2-ma/${ARCH}-linux/bin/mpif90
+export FCFLAGS="${FCFLAGS} -fexceptions -gcc-name=${CC} -gxx-name=${CXX} -nofor-main"
+#export LDFLAGS="${LDFLAGS} `/usr/pack/tibercad_dev-2.2-ma/${ARCH}-linux/bin/mpicxx -showme:link`"
+#export LDFLAGS="${LDFLAGS} `${PETSC_DIR}/externalpackages/mpich2-1.0.8/bin/mpicxx -showme:link`"
 
 
-
-#FCFLAGS="-fPIC ${FCFLAGS}"
-
-FORTRANDIR=/usr/pack/intel_fc-11.1-gp
+FORTRANDIR=/usr/pack/intel_ifort-13.0-ma
 export LDFLAGS="-Wl,-rpath,${FORTRANDIR}/lib/intel64"
 
-#  --with-tao-prefix=/usr/pack/tibercad_dev-2.1-ma/tao-1.10-p1 \
+BOOST="/usr/pack/tibercad_dev-2.2-ma/SDK"
+
+#CONFIGOPTS="--with-boost-prefix=$BOOST --with-boost-libdir=${BOOST}/${sepparch}/lib"
+CONFIGOPTS="--with-boost-prefix=$BOOST --with-boost-libdir=${BOOST}/${ARCH}-linux/lib"
+
+
+#  --enable-hetero \
+#  --enable-dftb \
+#  --with-mkl=/usr/pack/intel_mkl-10.2-gp \
+#  --enable-pardiso \
+#  --with-boost-prefix=/usr/pack/boost-1.33.1-ma \
+#  --with-tao-prefix=/usr/pack/tibercad_dev-2.2-ma/tao-1.10-p1 \
 
 ./configure \
-  --with-petsc-prefix=${SDKDIR}/build/petsc-3.0.0-p12 \
-  --with-petsc-arch=${ARCH}-complex \
-  --with-slepc-prefix=${SDKDIR}/build/slepc-3.0.0-p7 \
+  --with-petsc-prefix=${PETSC_DIR} \
+  --with-petsc-arch=${ARCH}-linux-complex \
+  --with-slepc-prefix=${SLEPC_DIR} \
   --with-mkl=/usr/pack/intel_mkl-10.2-gp \
-  --with-cuda=/usr/pack/cudatoolkit-5.5.11-ma \
   --with-subversion=svn-1.6.5 \
-  --enable-uptight \
+  --disable-license-check \
+  --enable-uptight\
   --enable-pardiso \
-  --with-boost-prefix=$SDKDIR --with-boost-libdir=${SDKDIR}/${ARCH}/lib \
-  --with-libmesh-petsc-libdir=${SDKDIR}/${ARCH}/lib \
-  --with-libmesh-prefix=${SDKDIR}/build/libmesh-svn-${ARCH}
+  ${CONFIGOPTS} \
+  --with-libmesh-prefix=/usr/pack/tibercad_dev-2.2-ma/extern/libmesh-svn-${ARCH}-linux \
+  --with-libmesh-petsc-libdir=/usr/pack/tibercad_dev-2.2-ma/${ARCH}-linux/lib
