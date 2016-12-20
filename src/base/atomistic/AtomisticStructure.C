33569205a7c7fc0da09bb29f930026e32308698e
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2016-12-20 10:49:25 +0000

    * a fix in AtomisticStructure

--- src/base/atomistic/AtomisticStructure.C
+++ src/base/atomistic/AtomisticStructure.C
@@ -1276,6 +1276,8 @@ AtomisticStructure::print_tgn(const std::string& path) const
 
   std::set<ID>::iterator ID_it;
 
+  // we need to know which regions have been used actually, otherwise
+  // we will write too many materials at the end.
   unsigned int id = 1;
   const Material* mat = NULL;
 
@@ -1300,10 +1302,6 @@ AtomisticStructure::print_tgn(const std::string& path) const
         }
       file << std::endl;
       
-      // we need to know which regions have been used actually, otherwise
-      // we will write too many materials at the end.
-      unsigned int id = 1;
-
       for (unsigned int i = 0; i < _atoms.size(); i++)
         {
           unsigned int n_specie;
