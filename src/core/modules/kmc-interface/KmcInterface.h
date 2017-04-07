b508ca1ace606ece26cb0dbca26addb730111065
Alessandro Pecchia <alessandro.pecchia@ismn.cnr.it>
2017-04-07 11:20:42 +0000

    adding kmc-interface module

---
+++ src/core/modules/kmc-interface/KmcInterface.h
@@ -0,0 +1,69 @@
+#ifndef _KMCINTERFACE_H_
+#define _KMCINTERFACE_H_
+
+#include "SimulationInterface.h"
+#include <tr1/random>
+#include "mesh.h"
+#include "elem.h"
+#include "TiberLinearSystem.h"
+#include "Boundary.h"
+#include "PotentialInterface.h"
+
+
+using namespace std;
+
+class TBDLLOCAL KmcInterface : public SimulationInterface
+{
+ public:
+
+  enum Solution
+  {
+    elDensity,
+    hlDensity,
+    xDensity,
+    eCurrentDensity,
+    hCurrentDensity,
+    Generation,
+    Recombination
+  };
+   
+  virtual ~KmcInterface() {};
+   
+ protected:
+ 
+  KmcInterface(const ModelOptions& opt) : SimulationInterface(opt){};
+ 
+  virtual void do_init();
+ 
+  //virtual void do_solve(){};
+ 
+  //virtual void parse_options(){};
+ 
+  virtual bool get_boundary_potentials(Boundary *bd, const Point& pt, double& phi, double& efermi, double& hfermi);
+  
+  SimulationInterface::SolutionProvider _pot_sol;
+  SimulationInterface::SolutionProvider _mue_sol;
+  SimulationInterface::SolutionProvider _muh_sol;
+
+  //! this system is used to assemble a fem useful for projections 
+  TiberLinearSystem* _sys;
+
+  vector<double> _potential;
+  
+  vector<double> _eldensity;
+
+  vector<double> _hldensity;
+
+  vector<double> _xdensity;
+
+  //! Monte Carlo variables
+  unsigned int _Nstep;
+
+  unsigned int _IOstep;
+
+};
+
+
+
+
+#endif
