74a819bb95879e8ce581d6113491163ebd064a33
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2017-08-30 13:28:09 +0000

    * eliminated additional solve after fail in Ramp

--- src/base/simulations/Ramp.C
+++ src/base/simulations/Ramp.C
@@ -202,9 +202,9 @@ Ramp::ramp(void)
       if (step == 0) throw SolveFailedException("Cannot decrease step size, is already 0");
 
       value = oldvalue;
-      VariableValue::set_variable_value(_variable, value);
-      for (int i = 0; i < num_sim; i++)
-        _simulations[i]->solve();
+      //VariableValue::set_variable_value(_variable, value);
+      //for (int i = 0; i < num_sim; i++)
+      //  _simulations[i]->solve();
     }
   }
   while (abs(_goal - value) > 1e9 * (abs(_goal) * numeric_limits<double>::epsilon()
