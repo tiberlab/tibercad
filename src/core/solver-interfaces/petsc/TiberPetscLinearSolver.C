88c49b6f80abc86e655ca3e2d9636bdd5eca6a46
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2017-09-07 12:26:32 +0000

    * eliminated debug output

--- src/core/solver-interfaces/petsc/TiberPetscLinearSolver.C
+++ src/core/solver-interfaces/petsc/TiberPetscLinearSolver.C
@@ -240,9 +240,9 @@ TiberPetscLinearSolver::do_solve(SparseMatrix<Number>&  matrix_in,
   // Solve the linear system
   ierr = KSPSolve(_ksp, rhs->vec(), solution->vec());
 
-  KSPConvergedReason reason;
-  KSPGetConvergedReason(_ksp, &reason);
-  std::cerr << "KSP convergence reason: " << reason << std::endl;
+  //KSPConvergedReason reason;
+  //KSPGetConvergedReason(_ksp, &reason);
+  //std::cerr << "KSP convergence reason: " << reason << std::endl;
   TiberPetscUtils::checkerr(ierr);
 
   return check_convergence();
