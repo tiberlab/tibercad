99f6c59fcdf77439d5214a99aa7a90696fbbd6f3
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2017-11-23 18:17:17 +0000

    * can now choose valley in kp density of states

--- src/core/semiconductormodels/ZbDDsemiconductor.C
+++ src/core/semiconductormodels/ZbDDsemiconductor.C
@@ -21,6 +21,9 @@ using namespace Constants;
 
     band_extremum  band_ex;
 
+    vector<string> valleys = {"Gamma", "X", "L"};
+    get_option("valley", valleys);
+
     //
     // there are 3 types of extrema: Gamma, X and L
     //
@@ -28,118 +31,130 @@ using namespace Constants;
     //---------------------------------------------------------
     // Gamma minimum
     //---------------------------------------------------------
-    double Ec_G = par.Ev + par.EgGamma;
+    auto it = std::find(valleys.begin(), valleys.end(), "Gamma");
+    if (it != valleys.end())
+    {
+      double Ec_G = par.Ev + par.EgGamma;
 
-    if (strained)  Ec_G  += par.a_c * trace(strain);
+      if (strained)  Ec_G  += par.a_c * trace(strain);
 
-    band_ex.degeneracy = 2   ; //spin
-    band_ex.energy     = Ec_G;
-    band_ex.mass_DOS   = par.m_G ; //mass is isotropic
+      band_ex.degeneracy = 2   ; //spin
+      band_ex.energy     = Ec_G;
+      band_ex.mass_DOS   = par.m_G ; //mass is isotropic
 
-    result.push_back(band_ex);
+      result.push_back(band_ex);
+    }
 
 
     //----------------------------------------------------------------
     // L minimum
     //----------------------------------------------------------------
-    double Ec_L = par.Ev + par.EgL;
-
-    if (strained)
+    it = std::find(valleys.begin(), valleys.end(), "L");
+    if (it != valleys.end())
     {
+      double Ec_L = par.Ev + par.EgL;
 
-      //Hydrostatic strain part--------------------------------
-      Ec_L  += (par.def_vol_L - (1.0/3.0)*par.def_uniax_L ) * trace(strain);
-      //Uniaxial strain part----------------------------------
-      Tensor1 k;
-      vector<double> dE_uniax(4);
-      k(1) = 1 ; k(2) =  1; k(3) =  1;
-      dE_uniax[0] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
-
-      k(1) = -1; k(2) =  1; k(3) =  1;
-      dE_uniax[1] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
-
-
-      k(1) =  1; k(2) = -1; k(3) =  1;
-      dE_uniax[2] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
-
-
-      k(1) =  1; k(2) =  1; k(3) = -1;
-      dE_uniax[3] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
-
-
-      double mass_DOS = pow(par.m_t_L * par.m_t_L *  par.m_l_L, 1.0/3.0 );
-      for (short i = 0; i <4; i++)
+      if (strained)
       {
-        band_ex.degeneracy = 4; //spin and k-> -k
-        band_ex.mass_DOS = mass_DOS;
 
-        band_ex.energy = Ec_L + dE_uniax[i];
-        result.push_back(band_ex);
+        //Hydrostatic strain part--------------------------------
+        Ec_L  += (par.def_vol_L - (1.0/3.0)*par.def_uniax_L ) * trace(strain);
+        //Uniaxial strain part----------------------------------
+        Tensor1 k;
+        vector<double> dE_uniax(4);
+        k(1) = 1 ; k(2) =  1; k(3) =  1;
+        dE_uniax[0] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
+
+        k(1) = -1; k(2) =  1; k(3) =  1;
+        dE_uniax[1] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
+
+
+        k(1) =  1; k(2) = -1; k(3) =  1;
+        dE_uniax[2] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
+
+
+        k(1) =  1; k(2) =  1; k(3) = -1;
+        dE_uniax[3] = par.def_uniax_L * (k * (strain * k)) * (1.0/3.0);
+
+
+        double mass_DOS = pow(par.m_t_L * par.m_t_L *  par.m_l_L, 1.0/3.0 );
+        for (short i = 0; i <4; i++)
+        {
+          band_ex.degeneracy = 4; //spin and k-> -k
+          band_ex.mass_DOS = mass_DOS;
+
+          band_ex.energy = Ec_L + dE_uniax[i];
+          result.push_back(band_ex);
+        }
+
       }
-
-    }
-    else
-    {
-      //band_ex.degeneracy = 16   ;
-      /* spin degeneracy and  8 equivalent minima (
+      else
+      {
+        //band_ex.degeneracy = 16   ;
+        /* spin degeneracy and  8 equivalent minima (
            [1,1, 1],[-1,-1,-1],
            [-1,1,1],[1,-1,-1]
            [1,-1,1],[-1,1,-1]
            [1,1,-1],[-1,-1,1]
-       */
+         */
 
-      // Matthias 2015-09-24: let's always put them explicitly
-      band_ex.degeneracy = 4   ;
-      band_ex.energy     = Ec_L;
-      band_ex.mass_DOS = pow(par.m_t_L * par.m_t_L *  par.m_l_L, 1.0/3.0 );
-      for (short i = 0; i <4; i++)
-        result.push_back(band_ex);
+        // Matthias 2015-09-24: let's always put them explicitly
+        band_ex.degeneracy = 4   ;
+        band_ex.energy     = Ec_L;
+        band_ex.mass_DOS = pow(par.m_t_L * par.m_t_L *  par.m_l_L, 1.0/3.0 );
+        for (short i = 0; i <4; i++)
+          result.push_back(band_ex);
 
+      }
     }
 
     //------------------------------------------------------------------
     // X minimum
     //------------------------------------------------------------------
-    double Ec_X = par.Ev + par.EgX;
-
-    if (strained)
+    it = std::find(valleys.begin(), valleys.end(), "X");
+    if (it != valleys.end())
     {
-      //Hydrostatic strain part--------------------------------
-      Ec_X  += (par.def_vol_X - (1.0/3.0)*par.def_uniax_X) * trace(strain);
-      //-------------------------------------------------------
-      //Uniaxial strain part----------------------------------
-      Tensor1 k;
-      vector<double> dE_uniax(3);
-      k(1) = 1 ; k(2) =  0; k(3) =  0;
-      dE_uniax[0] = par.def_uniax_X * (k * (strain * k));
+      double Ec_X = par.Ev + par.EgX;
 
-      k(1) = 0 ; k(2) =  1; k(3) =  0;
-      dE_uniax[1] = par.def_uniax_X * (k * (strain * k));
-
-      k(1) = 0 ; k(2) =  0; k(3) =  1;
-      dE_uniax[2] = par.def_uniax_X * (k * (strain * k));
-
-      double mass_DOS = pow(par.m_t_X * par.m_t_X *  par.m_l_X, 1.0/3.0 );
-
-      for (short i = 0; i <3; i++)
+      if (strained)
       {
-        band_ex.degeneracy = 4; //spin and k->-k
-        band_ex.mass_DOS = mass_DOS;
-        band_ex.energy = Ec_X + dE_uniax[i];
-        result.push_back(band_ex);
+        //Hydrostatic strain part--------------------------------
+        Ec_X  += (par.def_vol_X - (1.0/3.0)*par.def_uniax_X) * trace(strain);
+        //-------------------------------------------------------
+        //Uniaxial strain part----------------------------------
+        Tensor1 k;
+        vector<double> dE_uniax(3);
+        k(1) = 1 ; k(2) =  0; k(3) =  0;
+        dE_uniax[0] = par.def_uniax_X * (k * (strain * k));
+
+        k(1) = 0 ; k(2) =  1; k(3) =  0;
+        dE_uniax[1] = par.def_uniax_X * (k * (strain * k));
+
+        k(1) = 0 ; k(2) =  0; k(3) =  1;
+        dE_uniax[2] = par.def_uniax_X * (k * (strain * k));
+
+        double mass_DOS = pow(par.m_t_X * par.m_t_X *  par.m_l_X, 1.0/3.0 );
+
+        for (short i = 0; i <3; i++)
+        {
+          band_ex.degeneracy = 4; //spin and k->-k
+          band_ex.mass_DOS = mass_DOS;
+          band_ex.energy = Ec_X + dE_uniax[i];
+          result.push_back(band_ex);
+        }
+
+
       }
-
-
-    }
-    else
-    {
-      band_ex.energy     = Ec_X;
-      //band_ex.degeneracy = 12   ; // spin degeneracy and 6 equivalent minima
-      // Matthias 2015-09-24: let's always put them explicitly
-      band_ex.degeneracy = 4;
-      band_ex.mass_DOS = pow(par.m_t_X * par.m_t_X *  par.m_l_X, 1.0/3.0 );
-      for (short i = 0; i < 3; i++)
-        result.push_back(band_ex);
+      else
+      {
+        band_ex.energy     = Ec_X;
+        //band_ex.degeneracy = 12   ; // spin degeneracy and 6 equivalent minima
+        // Matthias 2015-09-24: let's always put them explicitly
+        band_ex.degeneracy = 4;
+        band_ex.mass_DOS = pow(par.m_t_X * par.m_t_X *  par.m_l_X, 1.0/3.0 );
+        for (short i = 0; i < 3; i++)
+          result.push_back(band_ex);
+      }
     }
 
     //------------------------------------------
