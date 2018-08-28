f7c366c74ee0ec9258a614bcd9793fc7e9a30053
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2018-08-28 15:14:47 +0000

    * delete debug output

--- src/base/common/TiberNonlinLS.C
+++ src/base/common/TiberNonlinLS.C
@@ -189,8 +189,8 @@ TiberNonlinLS::do_solve(void)
       //if (norm_du > get_divergence_tol() * norm_du_old)
       //  throw (SolveFailedException("Line search diverged"));
 
-      cerr << "       ||r(x)|| = " << norm_rhs << ", ||r(x + " <<
-        alpha << "*dx)|| = " << norm_res << endl;
+      //cerr << "       ||r(x)|| = " << norm_rhs << ", ||r(x + " <<
+      //  alpha << "*dx)|| = " << norm_res << endl;
 
 
       if ((norm_res < norm_rhs) || (norm_du < eps) || (norm_res < eps_res))
@@ -209,7 +209,7 @@ TiberNonlinLS::do_solve(void)
           double a = old_norm - norm_rhs - b;
 
           min_alpha = -a / (2 * b);
-          cerr << "         min: alpha = " << min_alpha << endl;
+          //cerr << "         min: alpha = " << min_alpha << endl;
           if ((min_alpha <= 1.0) && (min_alpha > 0))
             alpha = min_alpha;
         }
@@ -272,8 +272,8 @@ TiberNonlinLS::do_solve(void)
       double norm_res_old = norm_res;
       //norm_res = rhs->l2_norm();
       norm_res = TiberEqSystem::calculate_norm(rhs, l2_NORM);
-      cerr << "        ||r(x + " << alpha << "*dx)|| = " << norm_res_old <<
-        ", ||r(x + " << 0.5 * alpha << "*dx)|| = "  << norm_res << endl;
+      //cerr << "        ||r(x + " << alpha << "*dx)|| = " << norm_res_old <<
+      //  ", ||r(x + " << 0.5 * alpha << "*dx)|| = "  << norm_res << endl;
       if (norm_res > norm_res_old)
       {
         // keep the former step
