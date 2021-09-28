e3cf04311976190d3187a03340079711b3b00e18
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2021-09-28 06:57:10 +0000

    * the new configure_new.sh uses /usr/pack/tibercad_dev-3.2-ma,   with new compilers and newest boost

--- configure_new.sh
+++ configure_new.sh
@@ -1,43 +1,35 @@
-#!/bin/sh
+#!/bin/bash
 
 ARCH=`uname -m`
-if [ $ARCH == "i686" ]
-then
-  sepparch=i686-debian-linux4.0
-else
-  sepparch=amd64-debian-linux5.0
-  FCFLAGS="-fPIC"
-fi 
 
-BUILDDIR=/usr/pack/tibercad_dev-3.0-ma/build
-SDKDIR=/usr/pack/tibercad_dev-3.0-ma/SDK
-#BUILDDIR=/home/mpatria/newlibmesh2/extern
+BASEDIR=/usr/pack/tibercad_dev-3.2-ma
+BUILDDIR=${BASEDIR}/build
+SDKDIR=${BASEDIR}/SDK
+#PETSC_DIR=${BUILDDIR}/petsc-3.7.3
+#SLEPC_DIR=${BUILDDIR}/slepc-3.7.3
+#LIBMESHDIR=${BUILDDIR}/libmesh-1.0.0-${ARCH}-linux
 PETSC_DIR=${BUILDDIR}/petsc-3.6.2
 SLEPC_DIR=${BUILDDIR}/slepc-3.6.2
-LIBMESH_DIR=${BUILDDIR}/libmesh-0.9.5-${ARCH}-linux
-#BOOST="/usr/pack/tibercad_dev-3.0-ma/SDK"
-BOOST=${SDKDIR}
+LIBMESHDIR=${BUILDDIR}/libmesh-1.0.0-${ARCH}-linux
 
-
-#export CXX=g++-4.6.0
-#export CC=gcc-4.6.0
-#export FC=ifort-11.1
-export CXX=mpicxx-3.1.1
-export CC=mpicc-3.1.1
-export FC=mpif90-3.1.1
-export FCFLAGS="${FCFLAGS} -fexceptions -gcc-name=${CC} -gxx-name=${CXX} -nofor-main"
+export CXX=${SDKDIR}/${ARCH}-linux/bin/mpicxx
+export CC=${SDKDIR}/${ARCH}-linux/bin/mpicc
+export FC=${SDKDIR}/${ARCH}-linux/bin/mpifort
+#export FCFLAGS="${FCFLAGS} -fexceptions -gcc-name=${CC} -gxx-name=${CXX} -nofor-main"
 #export LDFLAGS="${LDFLAGS} `/usr/pack/tibercad_dev-2.2-ma/${ARCH}-linux/bin/mpicxx -showme:link`"
 #export LDFLAGS="${LDFLAGS} `${PETSC_DIR}/externalpackages/mpich2-1.0.8/bin/mpicxx -showme:link`"
 
-export CXXFLAGS="-std=gnu++11"
 
-FORTRANDIR=/usr/pack/intel_ifort-13.0-ma
+FORTRANDIR=/usr/pack/intel_oneapi-2021-ma/compiler/latest/linux/compiler
 export LDFLAGS="-Wl,-rpath,${FORTRANDIR}/lib/intel64"
+export FCFLAGS="-fexceptions -g -gcc-name=$(which ${CC}) -gxx-name=$(which ${CXX}) -nofor-main -qopenmp"
 
+BOOST="${SDKDIR}"
 
 #CONFIGOPTS="--with-boost-prefix=$BOOST --with-boost-libdir=${BOOST}/${sepparch}/lib"
 CONFIGOPTS="--with-boost-prefix=$BOOST --with-boost-libdir=${BOOST}/${ARCH}-linux/lib"
 
+SVN=svn-1.9.5
 
 #  --enable-hetero \
 #  --enable-dftb \
@@ -47,14 +39,18 @@ CONFIGOPTS="--with-boost-prefix=$BOOST --with-boost-libdir=${BOOST}/${ARCH}-linu
 #  --with-tao-prefix=/usr/pack/tibercad_dev-2.2-ma/tao-1.10-p1 \
 
 ./configure \
+  --with-cuda=/usr/pack/cudatoolkit-5.5.11-ma \
   --with-petsc-prefix=${PETSC_DIR} \
-  --with-petsc-arch=${ARCH}-linux-complex \
+  --with-petsc-arch=${ARCH}-linux \
   --with-slepc-prefix=${SLEPC_DIR} \
-  --with-mkl=/usr/pack/intel_mkl-11.2-ma/mkl \
-  --with-subversion=svn-1.6.5 \
-  --enable-license-check \
+  --with-mpiexec=mpiexec-3.1.1 \
+  --with-mkl=/usr/pack/intel_oneapi-2021-ma/mkl/latest \
+  --with-thread-library=intel \
+  --with-subversion=${SVN} \
+  --disable-license-check \
   --enable-uptight\
   --disable-pardiso \
   ${CONFIGOPTS} \
-  --with-libmesh-prefix=${LIBMESH_DIR} \
-  --with-libmesh-petsc-libdir=/home/pecchia/projects/tibercad/newlibmesh4/${ARCH}-linux/lib
+  --with-libmesh-prefix=${LIBMESHDIR} \
+  --with-cuda=/usr/pack/cudatoolkit-5.5.11-ma \
+  --with-libmesh-petsc-libdir=${SDKDIR}/${ARCH}-linux/lib
