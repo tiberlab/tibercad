7c71eab89b77a2d91acd2dadea63e7d020c4d357
Peyman Amiri <peymanamiri1992@gmail.com>
2023-10-09 09:20:34 +0000

    adding correct model interface for internal emission

---
+++ src/core/modules/tmm/models/OpticalRecombination.C
@@ -0,0 +1,58 @@
+
+#include "OpticalRecombination.h"
+#include "TiberModule.h"
+#include "SimulationInterface.h"
+#include "Messages.h"
+#include <string>
+
+
+
+void 
+OpticalRecombination::calculate(const libMesh::Elem* elem, const libMesh::Point& point, double lambda)
+{
+  string recombination_str(get_option("radiative_recombination", "0"));
+   vector<string> recombination;
+   Utils::extract_vector(recombination_str, recombination);
+   _recombination_model.resize(recombination.size());
+   _recombination_id.resize(recombination.size());
+   for (size_t i = 0; i < recombination.size(); ++i)
+    {
+      pair<SimulationInterface*, ID> provider =
+        SimulationInterface::find_solution_provider(recombination[i]);
+      _recombination_model[i] = provider.first;
+      _recombination_id[i] = provider.second;
+      if (_recombination_model[i] == NULL)
+        throw InitFailedException("Cannot find radiative_recombination model: " + recombination[i]);
+    }
+	
+    _recombination_rate = 0;
+	
+
+    vector<double> tmp(1);
+    for (size_t i = 0; i < _recombination_model.size(); ++i)
+    {
+      if (_recombination_model[i]->get_solution(elem, _recombination_id[i], tmp,
+          vector<Point>(1, point)))
+        _recombination_rate += tmp[0];
+    }
+	get_parameter("emission_peak", _emission_wavelength);
+	get_parameter("multiplier", _multiplier);
+	
+	if (_multiplier > 1 || _multiplier<= 0)
+		_multiplier = 1;
+	
+	
+	//std::cout << "_multiplier " << _multiplier << endl;
+	  
+	double plank_const = 6.62607015e-34;  //Planck constant[J*s]
+	double c0 = 2.998e8 * 1e9;  //speed of ligth [nm/s]
+	double power;
+	//energy of each photon multiplyed by number of recombinations
+	if ( _emission_wavelength == lambda)
+	 power = _recombination_rate * _multiplier ;// plank_const * (c0/lambda) * _recombination_rate /1000;  // [W/cm^2]
+    else 
+		power = 0;
+	
+	set_emission_power(power ); 
+}
+
---
+++ src/core/modules/tmm/models/OpticalRecombination.h
@@ -0,0 +1,65 @@
+// $Id: PermittivityModel.h 4729 2018-12-05 07:58:16Z maufder $
+
+#ifndef _OpticalRecombination_H_
+#define _OpticalRecombination_H_
+
+#include "Tmm.h"
+#include "TmmDipoleSource.h"
+
+namespace libMesh
+{
+  class Elem;
+}
+
+// Base class for InCoherence model
+class TBDLEXPORT OpticalRecombination : public TmmDipoleSource
+{
+
+  public:
+
+  virtual ~OpticalRecombination(void) {};
+  static OpticalRecombination* create(const ModelOptions& options);
+  virtual void calculate(const libMesh::Elem* elem, const libMesh::Point& point, double lambda);
+
+  
+protected:
+  
+    OpticalRecombination(const ModelOptions& options);
+	
+	virtual void do_init(void){};
+
+
+
+
+
+  private:
+
+   double _recombination_rate;
+   double _emission_wavelength;
+   double _multiplier=0;
+   
+       //! The generation model
+    std::vector<SimulationInterface*> _recombination_model;
+
+    //! The solution ID of the generation models variable
+    std::vector<ID> _recombination_id;
+
+};
+
+inline
+OpticalRecombination::OpticalRecombination(const ModelOptions& options) :
+  TmmDipoleSource(options),
+  _recombination_rate(0.0)
+{
+}
+
+inline
+OpticalRecombination*
+OpticalRecombination::create(const ModelOptions& options)
+{
+  return new OpticalRecombination(options);
+}
+
+
+
+#endif // _POLARIZATIONMODEL_H_
