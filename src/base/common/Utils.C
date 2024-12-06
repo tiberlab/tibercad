2e098005ee4798598d21291f82cd78bed468de9e
Matthias Auf der Maur <auf.der.maur@ing.uniroma2.it>
2024-12-06 12:00:08 +0000

    * bug fix in double vector extraction with ranges

--- src/base/common/Utils.C
+++ src/base/common/Utils.C
@@ -621,7 +621,7 @@ Utils::extract_vector(const string& input, vector<double>& vec)
         double sign = 1.0;
         if (step < 0) sign = -1.0;
 
-        for (double x = a; sign*x < (sign*b + 1e-3*step); x += step)
+        for (double x = a; sign*x < sign*(b - 1e-3*step); x += step)
           vec.push_back(x);
         vec.push_back(b);
       }
